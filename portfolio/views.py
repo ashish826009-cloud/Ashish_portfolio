04a541d66e96d9d3df2b18525f0ed00a617a4b33
BT04532 <ashish826009@gmail.com>
2025-12-18 17:29:02 +0530

    This is my portfolio and updates will be coming

---
+++ portfolio/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Contact
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+def index(request):
+
+    if request.method=="POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        contact = Contact(name=name,email=email,message=message)
+        contact.save()
+        send_mail(
+            subject=f"New Contact Message from {name}",
+            message=f"""
+You have received a new message from.
+
+Name: {name}
+Email: {email}
+
+Message:
+{message}
+            """,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=['ashishsikarwar268@gmail.com'],  # where you want to receive mail
+            fail_silently=False,
+        )
+    return render(request,'portfolio/index.html')
